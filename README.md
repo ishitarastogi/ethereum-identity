e4fef1917e0a758571f831737c08fc4118df94a0
ishitarastogi <ishita3099@gmail.com>
2022-01-31 11:58:47 +0530

    Initial setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ethereum-identity
+
+1. Ceramic : Decentralized network of nodes and you can write or read to these nodes
+2. IDX : It is an identity protocol and it integrates with ceramic abd it givves you an identity focussed way to integrate with ceramic
+3. 3ID Connect
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import {useState} from 'react';
+import { CeramicClient } from '@ceramicnetwork/http-client'
+import ThreeIdResolver from '@ceramicnetwork/3id-did-resolver'
+import { ThreeIdConnect,  EthereumAuthProvider } from '@3id/connect'
+import {DID} from 'dids'
+import {IDX} from '@ceramicstudio/idx'
+
+
+const endpoint = "https://ceramic-clay.3boxlabs.com"
+
+
+function App() {
+  return (
+    <div className="App">
+
+
+    </div>
+  );
+}
+
+export default App;
